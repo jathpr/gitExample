08b3c5c94f6f2699700a7a9b1a94740d4d23e661
jathpr <jathpr@gmail.com>
2020-03-19 16:11:35 +0300

    history for

---
+++ test.js
@@ -0,0 +1,17 @@
+let instr = "1";
+
+switch (instr) {
+  case "2": // instr === 2
+  case "1":
+    console.log("tandem");
+    break;
+
+  case 3: // instr === 2
+  case 1:
+    console.log("tandemerh");
+    break;
+
+  default:
+    console.log("ffa");
+    break;
+}
